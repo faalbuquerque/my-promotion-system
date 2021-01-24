31c32da8115052a12a47deeed4392cfdb6419aa3
Fabiane Albuquerque <bii.nett@gmail.com>
2021-01-24 06:30:22 -0300

    Add promotions registration tests with validations

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,2 +1,7 @@
 class Promotion < ApplicationRecord
+  validates :name, presence: true
+  validates :code, presence: true, uniqueness: true
+  validates :discount_rate, presence: true
+  validates :coupon_quantity, presence: true
+  validates :expiration_date, presence: true
 end
