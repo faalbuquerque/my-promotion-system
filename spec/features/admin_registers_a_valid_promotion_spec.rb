be87ccd0d0c60826758196130d1d145094913372
Fabiane Albuquerque <bii.nett@gmail.com>
2021-01-31 17:36:37 -0300

    Valid promotion feature test refactoring

--- spec/features/admin_registers_a_valid_promotion_spec.rb
+++ spec/features/admin_registers_a_valid_promotion_spec.rb
@@ -2,10 +2,6 @@ require 'rails_helper'
 
 feature 'Admin registers a valid promotion' do
   scenario 'and attributes cannot be blank' do
-    Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
-                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
-                      expiration_date: '22/12/2033')
-
     visit root_path
     click_on 'Promoções'
     click_on 'Registrar uma promoção'
