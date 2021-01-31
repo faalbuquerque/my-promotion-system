72194e67fac52636e5f13e8cd3a108e7a41cd990
Fabiane Albuquerque <bii.nett@gmail.com>
2021-01-31 18:56:57 -0300

    Refactoring the test to validate promotion and add translated error message

--- spec/features/admin_registers_a_valid_promotion_spec.rb
+++ spec/features/admin_registers_a_valid_promotion_spec.rb
@@ -13,7 +13,13 @@ feature 'Admin registers a valid promotion' do
     fill_in 'Data de término', with: ''
     click_on 'Criar promoção'
 
-    expect(page).to have_content('não pode ficar em branco', count: 5)
+    expect(Promotion.count).to eq 0
+    expect(page).to have_content('Não foi possível criar a promoção')
+    expect(page).to have_content('Nome não pode ficar em branco')
+    expect(page).to have_content('Código não pode ficar em branco')
+    expect(page).to have_content('Desconto não pode ficar em branco')
+    expect(page).to have_content('Quantidade de cupons não pode ficar em branco')
+    expect(page).to have_content('Data de término não pode ficar em branco')
   end
 
   scenario 'and code must be unique' do
